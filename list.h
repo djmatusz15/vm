6c554bc354345ebc3be356ff4782fef880c1ac4f
djmatusz15 <108953948+djmatusz15@users.noreply.github.com>
2024-06-25 12:23:04 -0400

    Create list.h

---
+++ list.h
@@ -0,0 +1,18 @@
+#ifndef FREELIST_H
+#define FREELIST_H
+
+#include <windows.h>
+#include <stdio.h>
+
+typedef struct page {
+    struct page* blink;
+    struct page* flink;
+    ULONG64 pfn;
+} page_t;
+
+page_t* instantiateFreeList(PULONG_PTR physical_frame_numbers, ULONG_PTR num_physical_frames);
+page_t* instantiateStandyList();
+page_t* popTailPage(page_t* listhead);
+void addToTail(page_t* listhead, ULONG64 given_pfn);
+
+#endif //FREELIST_H
